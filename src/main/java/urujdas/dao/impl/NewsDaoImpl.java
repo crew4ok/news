cddcab6edb4fe5266ca31f906a4f49caf298f662
Aleksandr Oksenenko <crew4ok@gmail.com>
2015-06-16 00:16:41 +0300

    Dao refactoring

--- src/main/java/urujdas/dao/impl/NewsDaoImpl.java
+++ src/main/java/urujdas/dao/impl/NewsDaoImpl.java
@@ -6,7 +6,7 @@ import org.jooq.Field;
 import org.jooq.Record;
 import org.jooq.SelectOnConditionStep;
 import org.jooq.SortField;
-import org.jooq.TableLike;
+import org.jooq.Table;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import urujdas.dao.NewsDao;
@@ -16,11 +16,17 @@ import urujdas.model.User;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
 import static org.jooq.impl.DSL.count;
 import static urujdas.tables.FavouritesTable.FAVOURITES;
 import static urujdas.tables.LikesTable.LIKES;
@@ -36,81 +42,79 @@ public class NewsDaoImpl implements NewsDao {
 
     @Override
     public News getById(Long id) {
-        Record record = selectClause()
-                .where(NEWS.ID.equal(id))
-                .groupBy(groupByClause())
-                .fetchOne();
-        if (record != null) {
-            return record.into(News.class);
-        } else {
-            return null;
+        List<News> result = select(
+                singletonList(NEWS.ID.equal(id)),
+                emptyMap(),
+                Optional.empty()
+        );
+        if (!result.isEmpty()) {
+            return result.get(0);
         }
+        return null;
     }
 
     private List<News> select(Collection<Condition> conditions,
-                              Collection<TableLike<?>> joins) {
+                              Map<Table<?>, Condition> joins,
+                              Optional<Integer> limit) {
 
-        List<Field<?>> fields = new ArrayList<>(Arrays.asList(NEWS.fields()));
-        fields.addAll(Arrays.asList(USERS.fields()));
-        fields.addAll(Arrays.asList(NEWS_CATEGORIES.fields()));
-        fields.add(count());
+        List<Field<?>> fields = new ArrayList<>(asList(NEWS.fields()));
+        fields.addAll(asList(USERS.fields()));
+        fields.addAll(asList(NEWS_CATEGORIES.fields()));
+        fields.add(count(LIKES.ID));
 
         SelectOnConditionStep<Record> step = ctx.select(fields)
                 .from(NEWS)
                 .join(USERS).on(NEWS.AUTHOR.equal(USERS.ID))
-                .join(NEWS_CATEGORIES).on(NEWS.CATEGORY_ID.equal(NEWS_CATEGORIES.ID));
+                .join(NEWS_CATEGORIES).on(NEWS.CATEGORY_ID.equal(NEWS_CATEGORIES.ID))
+                .leftOuterJoin(LIKES).on(NEWS.ID.equal(LIKES.NEWS_ID));
 
-        joins.forEach(step::join);
+        joins.forEach((t, c) -> step.join(t).on(c));
 
-        return step.where(conditions)
+        step.where(conditions)
                 .groupBy(groupByClause())
-                .orderBy(orderByIdClause())
-                .fetch()
+                .orderBy(orderByIdClause());
+
+        if (limit.isPresent()) {
+            step.limit(limit.get());
+        }
+        return step.fetch()
                 .into(News.class);
     }
 
     @Override
     public List<News> getLatestAll(int latestCount) {
-        return selectClause()
-                .groupBy(groupByClause())
-                .orderBy(orderByIdClause())
-                .limit(latestCount)
-                .fetch()
-                .into(News.class);
+        return select(
+                emptyList(),
+                emptyMap(),
+                Optional.of(latestCount)
+        );
     }
 
     @Override
     public List<News> getAllFromId(long id, int count) {
-        return selectClause()
-                .where(pagingWhereClause(id))
-                .groupBy(groupByClause())
-                .orderBy(orderByIdClause())
-                .limit(count)
-                .fetch()
-                .into(News.class);
+        return select(
+                singletonList(pagingWhereClause(id)),
+                emptyMap(),
+                Optional.of(count)
+        );
     }
 
     @Override
     public List<News> getLatestByUser(User user, int count) {
-        return selectClause()
-                .where(authorWhereClause(user))
-                .groupBy(groupByClause())
-                .orderBy(orderByIdClause())
-                .limit(count)
-                .fetch()
-                .into(News.class);
+        return select(
+                singletonList(authorWhereClause(user)),
+                emptyMap(),
+                Optional.of(count)
+        );
     }
 
     @Override
     public List<News> getByUserFromId(User user, Long id, int count) {
-        return selectClause()
-                .where(pagingWhereClause(id))
-                .and(authorWhereClause(user))
-                .groupBy(groupByClause())
-                .orderBy(orderByIdClause())
-                .limit(count)
-                .fetch()
-                .into(News.class);
+        return select(
+                asList(pagingWhereClause(id), authorWhereClause(user)),
+                emptyMap(),
+                Optional.of(count)
+        );
     }
 
     @Override
@@ -119,13 +123,11 @@ public class NewsDaoImpl implements NewsDao {
                 .map(User::getId)
                 .collect(Collectors.toList());
 
-        return selectClause()
-                .where(NEWS.AUTHOR.in(authorsIds))
-                .groupBy(groupByClause())
-                .orderBy(orderByIdClause())
-                .limit(count)
-                .fetch()
-                .into(News.class);
+        return select(
+                singletonList(NEWS.AUTHOR.in(authorsIds)),
+                emptyMap(),
+                Optional.of(count)
+        );
     }
 
     @Override
@@ -134,56 +136,33 @@ public class NewsDaoImpl implements NewsDao {
                 .map(User::getId)
                 .collect(Collectors.toList());
 
-        return selectClause()
-                .where(NEWS.AUTHOR.in(authorsIds))
-                .and(pagingWhereClause(id))
-                .groupBy(groupByClause())
-                .orderBy(orderByIdClause())
-                .limit(count)
-                .fetch()
-                .into(News.class);
+        return select(
+                asList(NEWS.AUTHOR.in(authorsIds), pagingWhereClause(id)),
+                emptyMap(),
+                Optional.of(count)
+        );
     }
 
     @Override
     public List<News> getLatestFavourites(User user, int count) {
-        return selectClause()
-                .join(FAVOURITES).on(FAVOURITES.NEWS_ID.equal(NEWS.ID))
-                .where(FAVOURITES.USER_ID.equal(user.getId()))
-                .groupBy(groupByClause())
-                .orderBy(orderByIdClause())
-                .limit(count)
-                .fetch()
-                .into(News.class);
+        return select(
+                singletonList(FAVOURITES.USER_ID.equal(user.getId())),
+                singletonMap(FAVOURITES, FAVOURITES.NEWS_ID.equal(NEWS.ID)),
+                Optional.of(count)
+        );
     }
 
     @Override
     public List<News> getFavouritesFromId(User user, Long id, int count) {
-        return selectClause()
-                .join(FAVOURITES).on(FAVOURITES.NEWS_ID.equal(NEWS.ID))
-                .where(FAVOURITES.USER_ID.equal(user.getId()))
-                .and(pagingWhereClause(id))
-                .groupBy(groupByClause())
-                .orderBy(orderByIdClause())
-                .limit(count)
-                .fetch()
-                .into(News.class);
-    }
-
-    private SelectOnConditionStep<?> selectClause() {
-        List<Field<?>> fields = new ArrayList<>(Arrays.asList(NEWS.fields()));
-        fields.addAll(Arrays.asList(USERS.fields()));
-        fields.addAll(Arrays.asList(NEWS_CATEGORIES.fields()));
-        fields.add(count(LIKES.ID));
-
-        return ctx.select(fields)
-                .from(NEWS)
-                .join(USERS).on(NEWS.AUTHOR.equal(USERS.ID))
-                .join(NEWS_CATEGORIES).on(NEWS.CATEGORY_ID.equal(NEWS_CATEGORIES.ID))
-                .leftOuterJoin(LIKES).on(NEWS.ID.equal(LIKES.NEWS_ID));
+        return select(
+                asList(FAVOURITES.USER_ID.equal(user.getId()), pagingWhereClause(id)),
+                singletonMap(FAVOURITES, FAVOURITES.NEWS_ID.equal(NEWS.ID)),
+                Optional.of(count)
+        );
     }
 
     private Collection<Field<?>> groupByClause() {
-        return Arrays.asList(NEWS.ID, USERS.ID, NEWS_CATEGORIES.ID);
+        return asList(NEWS.ID, USERS.ID, NEWS_CATEGORIES.ID);
     }
 
     private Condition authorWhereClause(User user) {
