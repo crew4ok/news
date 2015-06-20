88ab3de75049b47ee2245587804d6cdcdcdc778e
Aleksandr Oksenenko <crew4ok@gmail.com>
2015-06-20 22:58:56 +0300

    Fix service validation

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#Timeout needed for service to startup
+sleep 10
+
 response=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080/v1/healthcheck)
 if [ ${response} -eq '200' ]; then
     exit 0
