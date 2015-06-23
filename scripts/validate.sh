2253d9093fd8c0296bf8b5a8d660a75481551c12
Aleksandr Oksenenko <crew4ok@gmail.com>
2015-06-23 16:31:18 +0300

    Add retries to service validation script

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 #Timeout needed for service to startup
-sleep 10
+retries=5
 
-response=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080/v1/healthcheck)
-if [ ${response} -eq '200' ]; then
-    exit 0
-else
-    exit 1
-fi
+result=0
+for i in {1..${retries}}; do
+    sleep 10
+
+    response=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080/v1/healthcheck)
+    if [ ${response} -ne '200' ]; then
+        result=1
+        echo 'Response: '${response}
+    fi
+done
+
+exit ${result}
