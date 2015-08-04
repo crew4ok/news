2e762b5057266c4bf761f1d5eee4baec246ea6e3
Aleksandr Oksenenko <crew4ok@gmail.com>
2015-08-04 17:32:20 +0300

    Fix current user retrieval if logined with login/pass

--- src/main/java/urujdas/service/impl/UserServiceImpl.java
+++ src/main/java/urujdas/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,7 +41,7 @@ public class UserServiceImpl implements UserService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication instanceof UsernamePasswordAuthenticationToken) {
-            String username = (String) authentication.getPrincipal();
+            String username = ((UserDetails) authentication.getPrincipal()).getUsername();
 
             return userDao.getByUsername(username);
          } else if (authentication instanceof SocialNetworkUserAuthentication) {
