2bc121519ffc7ff5a5e91dc9ab8abbfc4798ad78
Aleksandr Oksenenko <crew4ok@gmail.com>
2015-08-21 22:55:02 +0300

    Fix deployment issues

--- flyway/deltas/V6__ads_type.sql
+++ flyway/deltas/V6__ads_type.sql
@@ -4,10 +4,10 @@ CREATE TABLE ads_types (
   category_id BIGINT REFERENCES ads_categories (id)
 );
 
-ALTER TABLE ads
-  ALTER COLUMN ads_type TYPE BIGINT USING NULL,
-  ADD FOREIGN KEY (ads_type) REFERENCES ads_types (id);
+DELETE FROM ads;
 
 ALTER TABLE ads
-    ALTER COLUMN subcategory_id SET NOT NULL,
-    ALTER COLUMN author SET NOT NULL;
+  ALTER COLUMN ads_type DROP NOT NULL,
+  ALTER COLUMN ads_type TYPE BIGINT USING NULL,
+  ALTER COLUMN ads_type SET NOT NULL,
+  ADD FOREIGN KEY (ads_type) REFERENCES ads_types (id);
